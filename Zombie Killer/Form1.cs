1f5ba2231b52dbf1fbf27c5678056803e8d8c045
Sabrina Tan <sabrinatzt2000@gmail.com>
2020-11-06 15:43:37 +0800

    Adding comments and create medic list

--- Zombie Killer/Form1.cs
+++ Zombie Killer/Form1.cs
@@ -28,7 +28,8 @@ namespace Zombie_Killer
         int testing;
         Random rnd = new Random(); // this is an instance of the random class we will use this to create a random number for this game
 
-        List<PictureBox> zombiesList = new List<PictureBox>();
+        List<PictureBox> zombiesList = new List<PictureBox>(); // list to store the zombie
+        List<PictureBox> medicList = new List<PictureBox>(); // list to store the medic
 
 
         public Form1()
@@ -38,7 +39,7 @@ namespace Zombie_Killer
         }
 
         private void MedicTimerEvent(object sender, EventArgs e)
-        {
+        {   // drop the medic after every 10 seconds
             DropMedic();
         }
 
@@ -81,40 +82,41 @@ namespace Zombie_Killer
 
             foreach (Control x in this.Controls)
             {
-                if (x is PictureBox && (string)x.Tag == "ammo")
+                if (x is PictureBox && (string)x.Tag == "ammo") 
                 {
-                    if (player.Bounds.IntersectsWith(x.Bounds))
+                    if (player.Bounds.IntersectsWith(x.Bounds)) // if the player intersects with ammor 
                     {
-                        this.Controls.Remove(x);
-                        ((PictureBox)x).Dispose();
-                        ammo += 5;
+                        this.Controls.Remove(x); // remove the ammo on screen
+                        ((PictureBox)x).Dispose(); // dispose the picture box
+                        ammo += 5; // add the ammo to 5
                     }
                 }
 
                 if (x is PictureBox && (string)x.Tag == "medic")
                 {
-                    if (player.Bounds.IntersectsWith(x.Bounds))
+                    if (player.Bounds.IntersectsWith(x.Bounds)) // if the player intersects with medic 
                     {
-                        this.Controls.Remove(x);
-                        ((PictureBox)x).Dispose();
+                        this.Controls.Remove(x); // remove the medic on screen
+                        medicList.Remove((PictureBox)x); // remove the medic from the medic list
+                        ((PictureBox)x).Dispose(); // dispose the picture box
                         if ((playerHealth + 30) <= 100)
                         {
-                            playerHealth += 30;
+                            playerHealth += 30; // increase the player health by 30
                         }
                         else
                         {
-                            playerHealth = 100;
+                            playerHealth = 100; // if the player's health + 30 is greater than 100, player' health = 100
                         }
                     }
                 }
 
                 if (x is PictureBox && (string)x.Tag == "zombie")
                 {
-                    if (player.Bounds.IntersectsWith(x.Bounds))
+                    if (player.Bounds.IntersectsWith(x.Bounds)) // if the player's intersects with zombie
                     {
-                        playerHealth -= 1;
+                        playerHealth -= 1; //player's health minus by 1
                     }
-
+                    // let the zombie chase the player
                     if (x.Left > player.Left)
                     {
                         x.Left -= zombieSpeed;
@@ -138,19 +140,19 @@ namespace Zombie_Killer
                 }
 
                 foreach (Control j in this.Controls)
-                {
+                {  
                     if (j is PictureBox && (string)j.Tag == "bullet" && x is PictureBox && (string)x.Tag == "zombie")
                     {
-                        if (x.Bounds.IntersectsWith(j.Bounds))
+                        if (x.Bounds.IntersectsWith(j.Bounds)) // if bullets are intersect with zombies
                         {
-                            score++;
+                            score++; // score plus 1
 
-                            this.Controls.Remove(j);
-                            ((PictureBox)j).Dispose();
-                            this.Controls.Remove(x);
-                            ((PictureBox)x).Dispose();
-                            zombiesList.Remove(((PictureBox)x));
-                            MakeZombies();
+                            this.Controls.Remove(j); // remove the bullets on screen
+                            ((PictureBox)j).Dispose(); // dispose the picture box
+                            this.Controls.Remove(x); // remove the zombie on screen
+                            ((PictureBox)x).Dispose(); // dispose the picture box
+                            zombiesList.Remove(((PictureBox)x)); // remove the zombie from the list
+                            MakeZombies(); // create the zombies
                         }
                     }
                 }
@@ -181,18 +183,18 @@ namespace Zombie_Killer
 
             if (e.KeyCode == Keys.Space && ammo > 0 && gameOver == false)
             {
-                ammo--;
+                ammo--; // deduct the ammo when player hit space key
                 ShootBullet(facing);
 
                 if (ammo < 1)
                 {
-                    DropAmmo();
+                    DropAmmo(); // drop the ammo when ammo is 0
                 }
             }
 
-           if (e.KeyCode == Keys.Enter && gameOver == true)
-           {
-                RestartGame();
+           if (e.KeyCode == Keys.Enter && gameOver == true) // player hits enter key and gameover is true
+            {
+                RestartGame(); // restart the game
            }
         }
 
@@ -207,35 +209,35 @@ namespace Zombie_Killer
             {
                 goleft = true;
                 facing = "left";
-                player.Image = Properties.Resources.left;
+                player.Image = Properties.Resources.left; // player faces left
             }
 
             if (e.KeyCode == Keys.Right)
             {
                 goright = true;
                 facing = "right";
-                player.Image = Properties.Resources.right;
+                player.Image = Properties.Resources.right; // player faces right
             }
 
             if (e.KeyCode == Keys.Up)
             {
                 goup = true;
                 facing = "up";
-                player.Image = Properties.Resources.up;
+                player.Image = Properties.Resources.up; // player faces up
             }
 
             if (e.KeyCode == Keys.Down)
             {
                 godown = true;
                 facing = "down";
-                player.Image = Properties.Resources.down;
+                player.Image = Properties.Resources.down; // player faces down
             }
         }
 
         private void ShootBullet(string direction)
         {
-            bullet shootBullet = new bullet();
-            shootBullet.direction = direction;
+            bullet shootBullet = new bullet(); // create the bullet
+            shootBullet.direction = direction; // set the direction to player's direction
             shootBullet.bulletLeft = player.Left + (player.Width / 2);
             shootBullet.bulletTop = player.Top + (player.Height / 2);
             shootBullet.MakeBullet(this);
@@ -247,24 +249,24 @@ namespace Zombie_Killer
             zombie.Tag = "zombie";
             zombie.Image = Properties.Resources.zdown;
             zombie.Left = rnd.Next(0, 900);
-            zombie.Top = rnd.Next(0, 800);
+            zombie.Top = rnd.Next(0, 800); 
             zombie.SizeMode = PictureBoxSizeMode.AutoSize;
-            zombiesList.Add(zombie);
-            this.Controls.Add(zombie);
-            player.BringToFront();
+            zombiesList.Add(zombie); // add zombies to the list
+            this.Controls.Add(zombie); // add zombies to the screen
+            player.BringToFront(); // bring the player to the front
         }
 
         private void DropAmmo()
         {
             PictureBox ammo = new PictureBox();
-            ammo.Image = Properties.Resources.ammo_Image;
+            ammo.Image = Properties.Resources.ammo_Image; 
             ammo.SizeMode = PictureBoxSizeMode.AutoSize;
             ammo.Left = rnd.Next(10, this.ClientSize.Width - ammo.Width);
             ammo.Top = rnd.Next(60, this.ClientSize.Height - ammo.Height);
             ammo.Tag = "ammo";
-            this.Controls.Add(ammo);
-            ammo.BringToFront();
-            player.BringToFront();
+            this.Controls.Add(ammo); // add the ammo to the screen
+            ammo.BringToFront(); // bring the ammo to the front
+            player.BringToFront(); // bring the player to the front
         }
 
         private void DropMedic()
@@ -274,25 +276,32 @@ namespace Zombie_Killer
             medic.SizeMode = PictureBoxSizeMode.AutoSize;
             medic.Left = rnd.Next(15, this.ClientSize.Width - medic.Width);
             medic.Top = rnd.Next(60, this.ClientSize.Height - medic.Height);
+            medicList.Add(medic); // add the medic to the list
             medic.Tag = "medic";
-            this.Controls.Add(medic);
-            medic.BringToFront();
-            player.BringToFront();
+            this.Controls.Add(medic); // add the medic to the screen
+            medic.BringToFront(); // bring the medic to the front
+            player.BringToFront(); // bring the player to the front
         }
 
         private void RestartGame()
         {
-            player.Image = Properties.Resources.up;
+            player.Image = Properties.Resources.up; // set player faces up
 
             foreach(PictureBox i in zombiesList)
             {
-                this.Controls.Remove(i);
+                this.Controls.Remove(i); // remove all the zombies on the screen
             }
-            zombiesList.Clear();
+            zombiesList.Clear(); // clear the zombieList
 
-            for(int i = 0; i < 3; i++)
+            foreach (PictureBox i in medicList)
             {
-                MakeZombies();
+                this.Controls.Remove(i); // remove all the medics on the screen
+            }
+            medicList.Clear(); // clear the medicList
+
+            for (int i = 0; i < 3; i++)
+            {
+                MakeZombies(); // create 3 zombies on the screen
             }
 
             goup = false;
